ae55535a642e287475b5a8f065897baffdbf4ae1
Marisha <mar.salikhova@gmail.com>
2018-11-11 23:17:52 +0200

    Painted Philosophical Triangle

---
+++ Triangle.py
@@ -0,0 +1,29 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> t = turtle.Pen()
+>>> t.forward(100)
+>>> t.left(315)
+>>> t.right (315)
+>>> t.rigth (315)
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    t.rigth (315)
+AttributeError: 'Turtle' object has no attribute 'rigth'
+>>> t.right (315)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.forward (50)
+>>> t.forward (50)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.right (45)
+>>> t.forward (100)
+>>> 
